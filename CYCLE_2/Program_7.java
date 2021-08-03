d0c5e1aca35d8c8e8c60cfc910a3c57fd7661e35
THEERTHA T <74696229+THEERTHA-T@users.noreply.github.com>
2021-08-03 23:04:55 +0530

    Add files via upload

---
+++ CYCLE_2/Program_7.java
@@ -0,0 +1,61 @@
+//question 12
+
+import java.util.*;
+class Book
+{
+	String title,author,pub;
+	int price,isbn;
+	void read()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("\nEnter ISBN :");
+		isbn=Integer.parseInt(sc.nextLine());
+		System.out.print("Enter title:");
+		title=sc.nextLine();
+		System.out.print("Enter Author:");
+		author=sc.nextLine();
+		System.out.print("Enter price:");
+		price=Integer.parseInt(sc.nextLine());
+		System.out.print("Enter publisher:");
+		pub=sc.nextLine();
+	}
+	void disp()
+	{
+		System.out.println("\nISBN\tTITLE\tAUTHOR\tPRICE\tPUBLISHER");
+		System.out.println(isbn+"\t"+title+"\t"+author+"\t"+price+"\t"+pub);
+		
+	}
+}
+class Program_7
+{
+	public static void main(String args[])
+	{
+		Book b[]=new Book[5];
+		System.out.println("\nEnter Book Details");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("\nBOOK "+(i+1)); 
+			b[i]=new Book();
+			b[i].read();
+		}
+		for(int i=0;i<5;i++)
+		{
+			for(int j=i+1;j<5;j++)
+			{
+				if(b[i].title.compareTo(b[j].title)>0)
+				{
+					Book temp;
+					temp=b[i];
+					b[i]=b[j];
+					b[j]=temp;
+				}
+			}
+		}
+		System.out.println("\n---DISPLAY DETAILS---\n");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("\n---BOOK "+(i+1)+"---");
+			b[i].disp();
+		}
+	}
+}
